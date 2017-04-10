2d684f21d95acb5d82a3984aa2f26d2eb50bdde0
KolorowyAleksander <KolorowyAleksander@users.noreply.github.com>
2017-04-11 00:36:41 +0200

    hello world template

---
+++ src/main/java/io/riders/RidersApplication.java
@@ -0,0 +1,12 @@
+package io.riders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidersApplication.class, args);
+	}
+}
