8beb5cbaa3b27b91292e695cf628617fd62f3aca
Marcin Mrugas <mrugacz95@gmail.com>
2017-04-13 01:48:37 +0200

    Added comments

--- src/main/java/io/riders/CoinController.java
+++ src/main/java/io/riders/CoinController.java
@@ -4,10 +4,14 @@ import io.riders.models.CoinModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+/**
+ * Controller class for coin page
+ */
 @Controller
 public class CoinController {
-
+    /**
+     * @return CoinModel initized with random number
+     */
     @RequestMapping("/coin")
     public @ResponseBody CoinModel coinToss() {
         return new CoinModel((int)Math.round(Math.random()));
