445143600e2c5bd7bd658506c8a5e754b7500084
Tommy <tomi.git@gmail.com>
2017-05-05 15:48:09 +0200

    Add Role And User Serive imterfaces

---
+++ src/main/java/io/riders/services/RoleService.java
@@ -0,0 +1,9 @@
+package io.riders.services;
+
+import io.riders.models.Role;
+
+/**
+ * Created by tommy on 5/5/2017.
+ */
+public interface RoleService extends CRUDService<Role>{
+}
---
+++ src/main/java/io/riders/services/UserService.java
@@ -0,0 +1,9 @@
+package io.riders.services;
+
+import io.riders.models.User;
+
+/**
+ * Created by tommy on 5/5/2017.
+ */
+public interface UserService extends CRUDService<User>{
+}
