df349a9ad2e1dbceb63edaf5f4502f207ef490b7
Tommy <tomi.git@gmail.com>
2017-05-05 16:19:44 +0200

    Add configuraion class CommonBeanConfig
    It is a class for configuration of strong password encryption

---
+++ src/main/java/io/riders/config/CommonBeanConfig.java
@@ -0,0 +1,18 @@
+package io.riders.config;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by tommy on 5/5/2017.
+ */
+@Configuration
+public class CommonBeanConfig {
+
+    @Bean
+    public StrongPasswordEncryptor strongEncryptor() {
+        StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+        return encryptor;
+    }
+}
