fdc1b53c807f3a968afda198a5ef4b183fd15354
Tommy <tomi.git@gmail.com>
2017-05-05 16:35:29 +0200

    Add UserDetailsService implementation

--- src/main/java/io/riders/services/security/UserDetailsServiceImpl.java
+++ src/main/java/io/riders/services/security/UserDetailsServiceImpl.java
@@ -1,9 +1,34 @@
 package io.riders.services.security;
 
+import io.riders.models.User;
+import io.riders.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Created by tommy on 5/5/2017.
  */
 public class UserDetailsServiceImpl implements UserDetailsService{
+    private UserService userService;
+    private Converter<User, UserDetails> userUserDetailsConverter;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    @Qualifier(value = "userToUserDetails")
+    public void setUserUserDetailsConverter(Converter<User, UserDetails> userUserDetailsConverter) {
+        this.userUserDetailsConverter = userUserDetailsConverter;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userUserDetailsConverter.convert(userService.findByUsername(username));
+    }
 }
