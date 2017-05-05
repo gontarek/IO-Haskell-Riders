bb01aa069f1c15a1f1cd493585d3731893c77cf9
Tommy <tomi.git@gmail.com>
2017-05-05 16:34:07 +0200

    Add UserToUserDetails converter

---
+++ src/main/java/io/riders/services/security/UserDetailsServiceImpl.java
@@ -0,0 +1,9 @@
+package io.riders.services.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by tommy on 5/5/2017.
+ */
+public class UserDetailsServiceImpl implements UserDetailsService{
+}
