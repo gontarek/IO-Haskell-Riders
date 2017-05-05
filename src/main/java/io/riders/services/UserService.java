f0f8453122af2655adb4260a5ffa775a1bdad2d4
Tommy <tomi.git@gmail.com>
2017-05-05 16:14:32 +0200

    Add UserServiceImpl class

--- src/main/java/io/riders/services/UserService.java
+++ src/main/java/io/riders/services/UserService.java
@@ -6,4 +6,5 @@ import io.riders.models.User;
  * Created by tommy on 5/5/2017.
  */
 public interface UserService extends CRUDService<User>{
+    User findByUsername(String username);
 }
---
+++ src/main/java/io/riders/services/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package io.riders.services;
+
+import io.riders.models.User;
+import io.riders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tommy on 5/5/2017.
+ */
+public class UserServiceImpl implements UserService{
+    private UserRepository userRepository;
+
+    @Autowired
+    private void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    @Override
+    public User getById(Integer id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public User saveOrUpdate(User domainObject) {
+        return userRepository.save(domainObject);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        userRepository.delete(id);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
