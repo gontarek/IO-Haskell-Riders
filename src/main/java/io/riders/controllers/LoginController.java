9bde159bafe31c42aeefdff011cc28b309737dbd
Tommy <tomi.git@gmail.com>
2017-05-06 16:10:26 +0200

    Add login view and controller

---
+++ src/main/java/io/riders/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package io.riders.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by tommy on 5/6/2017.
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+}
