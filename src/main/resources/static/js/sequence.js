04f1a7027a34a1b05d3d3a13aca11dd45607f1cf
Marcin Mrugas <mrugacz95@gmail.com>
2017-06-14 19:09:05 +0200

    ja sequence

--- src/main/resources/static/js/sequence.js
+++ src/main/resources/static/js/sequence.js
@@ -1,10 +1,18 @@
 /**
  * Created by mruga on 11.06.2017.
  */
+function load_sequence(){
+    $.ajax({
+        url: 'sequence'
+    }).done(function(data) {
+        $('#seq').text(data)
+    });
+}
 $(document).ready(function() {
-        $.ajax({
-            url: 'sequence'
-        }).done(function(data) {
-            $('#seq').text(data)
-        });
+    load_sequence();
 });
+$('#load_sequence').click(
+    function () {
+        load_sequence();
+    }
+);
